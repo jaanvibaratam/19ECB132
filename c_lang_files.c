08ba2f3a9b24afaf22721666320b922f3017df9b
jaanvi baratam <101267617+jaanvibaratam@users.noreply.github.com>
2022-05-30 14:09:42 +0000

    clangfiles

--- c_lang_files.c
+++ c_lang_files.c
@@ -1,7 +1,9 @@
 //program to display details
 #include <stdio.h>
- main()
+ int main()
  {
-    printf("b jaanvi")
-    printf("7075665508")
+    printf("b jaanvi");
+    printf("7075665508");
  }
+/*output
+b jaanvi7075665508 */
