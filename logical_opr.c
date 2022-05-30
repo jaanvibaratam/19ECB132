8ed95158365c7caed159f6c3f7db722e6dd84077
jaanvi baratam <101267617+jaanvibaratam@users.noreply.github.com>
2022-05-30 14:05:24 +0000

    logical opr

--- logical_opr.c
+++ logical_opr.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-main()
+int main()
 {
 	int a=2,b=3,c=4;
 	printf("(a<b&&a<c)=%d",(a<b&&a<c));
@@ -7,3 +7,5 @@ main()
 	printf("(a=>b||b==c)=%d",(a==b||b==c));
 	printf("(a!=b||b=c)=%d",(a!=b||b==c));
 }
+/* output 
+(a<b&&a<c)=1(c<b&&c<a)=0(a=>b||b==c)=0(a!=b||b=c)=1 */
