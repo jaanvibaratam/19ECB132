1e6b219901262e3d140021f7af903a4a73e2606a
jaanvi baratam <101267617+jaanvibaratam@users.noreply.github.com>
2022-05-31 05:18:07 +0000

    ....

--- linked_list.c
+++ linked_list.c
@@ -94,3 +94,29 @@ int main() {
   }
     
 }
+/* output
+1. Singly Linked List 
+2. Doubly Linked List 
+3. Circular Linked List 
+4. Exit
+Choose an option: 11
+
+Invalid Option
+1. Singly Linked List 
+2. Doubly Linked List 
+3. Circular Linked List 
+4. Exit
+Choose an option: 1
+
+*** Singly Linked List ***
+Enter node data: 2
+
+To insert a new node press 1 else any other integer: 2
+
+The elements in the Singly Linked list are...
+2
+1. Singly Linked List 
+2. Doubly Linked List 
+3. Circular Linked List 
+4. Exit
+Choose an option: 4 */
